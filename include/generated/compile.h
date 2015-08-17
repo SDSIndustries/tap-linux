b51ab133834abc77e3b039fe7a7f4956f45ed87c
scott <scott@kilncontrol.com>
2015-08-17 12:03:57 -0400

    Update Splash

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,6 +1,6 @@
-/* This file is auto generated, version 6 */
+/* This file is auto generated, version 13 */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 Sun Jun 21 14:38:17 EDT 2015"
+#define UTS_VERSION "#13 Sun Aug 16 16:41:22 EDT 2015"
 #define LINUX_COMPILE_BY "scott"
 #define LINUX_COMPILE_HOST "SDSM4800"
 #define LINUX_COMPILER "gcc version 4.7.3 (Ubuntu/Linaro 4.7.3-12ubuntu1) "
