9d633c0071f8543bc4b16d1cce4d4330d55f9a1e
scott <scott@kilncontrol.com>
2016-09-29 21:04:57 -0400

    Finale uppdates for Rev 3.0 software

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,6 +1,6 @@
-/* This file is auto generated, version 58 */
+/* This file is auto generated, version 64 */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#58 Sat Aug 6 18:29:36 EDT 2016"
+#define UTS_VERSION "#64 Thu Sep 29 21:00:17 EDT 2016"
 #define LINUX_COMPILE_BY "scott"
 #define LINUX_COMPILE_HOST "SDSM4800"
 #define LINUX_COMPILER "gcc version 4.7.3 (Ubuntu/Linaro 4.7.3-12ubuntu1) "
