ad5eb3e32d3102a1a9db10049787868d5507bd67
Scott <scott@kilncontrol.com>
2018-10-03 16:05:31 -0400

    AD7799 final - Evenheat logo

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,6 +1,6 @@
-/* This file is auto generated, version 179 */
+/* This file is auto generated, version 180 */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#179 Thu Sep 20 22:24:24 EDT 2018"
+#define UTS_VERSION "#180 Wed Oct 3 16:04:04 EDT 2018"
 #define LINUX_COMPILE_BY "scott"
 #define LINUX_COMPILE_HOST "SDSM4800"
 #define LINUX_COMPILER "gcc version 4.7.3 (Ubuntu/Linaro 4.7.3-12ubuntu1) "
